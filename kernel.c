df126d00ea5a8798e68a5166b13f9fbbfee87a07
Hwanhee Jo <hwanheejo@Hwanheeui-MacBookPro-2.local>
2025-01-18 18:02:46 +0900

    fix: entry point

--- kernel.c
+++ kernel.c
@@ -25,6 +25,21 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
+
+extern char __bss[], __bss_end[], __stack_top[];
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+
 void kernel_main(void) {
     const char *s = "\n\nHello World!\n";
     for (int i = 0; s[i] != '\0'; i++) {
@@ -35,3 +50,16 @@ void kernel_main(void) {
         __asm__ __volatile__("wfi");
     }
 }
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n" // Set the stack pointer
+        "j kernel_main\n"       // Jump to the kernel main function
+        :
+        : [stack_top] "r" (__stack_top) // Pass the stack top address as %[stack_top]
+    );
+}
+
+
